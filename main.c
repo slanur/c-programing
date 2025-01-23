ebe23d3c6d55eec1b33563576fec0f242e992c56
Sılanur ÇANDIR <silanurcndr52@gmail.com>
2025-01-23 17:37:53 +0300

    0.0

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello world!
+//
+//  Created by SILANUR ÇANDIR on 9.11.2024.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    printf("I learn C.\n");
+    printf("This is my first code in C\n");
+    return 0;
+}
