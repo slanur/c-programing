0197c22dce5e6241ca2941f2e15252a01ee8e39c
Sılanur ÇANDIR <silanurcndr52@gmail.com>
2025-02-12 14:56:42 +0300

    Add files via upload

---
+++ 00/asterix sembolleri.c
@@ -0,0 +1,30 @@
+//
+//  main.c
+//  asterix symbols
+//
+//  Created by SILANUR ÇANDIR on 9.11.2024.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    printf("*********\n");
+    printf("*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("*********\n\n");
+
+    
+    printf("     *\n");
+    printf("  *    *\n");
+    printf(" *\t\t*\n *\t\t*\n *\t\t*\n *\t\t*\n *\t\t*\n");
+    printf("  *    *\n");
+    printf("     *\n\n");
+    
+    
+    return 0;
+}
