1451e85e9be9f0622ff89ebc84f2ec6243ebafea
Sılanur ÇANDIR <silanurcndr52@gmail.com>
2025-04-10 11:23:28 +0300

    Update karakterİşlemeKütüphanesi.c

--- 08/karakterİşlemeKütüphanesi.c
+++ 08/karakterİşlemeKütüphanesi.c
@@ -10,6 +10,7 @@ ilk önce <ctype.h> kütüphanesini çağırıyoruz
 ->int isblank(int c); c karakteri bir girdideki kelimeleri ayıran boşluk karakteri ise true(1) yanlış ise false(0) değeri döndürür
  ->int isdigit(int c); c karakteri rakamsa true değilse false
  ->int isalpha(int c); c karakteri harfse true değilse false
+ -->int ispuntc(int c); c karakteri noktalama işaretiyse true
  ->int isalnum(int c); c karakteri harf veya rakamsa true değilse false
  daha fazlası kitapta sayfa 338 de
  ayrıca bunlar bir fonksiyon, o yüzden kullanırken başında int kullanmaya gerek yok
